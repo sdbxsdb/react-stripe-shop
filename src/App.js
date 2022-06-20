610144891608038f7c2fb61417a7e64206fc22d8
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-06-20 11:40:38 +0100

    before installing sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 
 const App = () => {
-  return <div className="App">Stripe shop</div>;
+  return (
+  <div className="categories-container">
+    <div className="category-container">
+      {/* <img src="" alt="" /> */}
+      <div className='category-body-container'>
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img src="" alt="" /> */}
+      <div className='category-body-container'>
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img src="" alt="" /> */}
+      <div className='category-body-container'>
+        <h2>Shoes</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img src="" alt="" /> */}
+      <div className='category-body-container'>
+        <h2>Men</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* <img src="" alt="" /> */}
+      <div className='category-body-container'>
+        <h2>Women</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  </div>);
 }
 
 export default App;
