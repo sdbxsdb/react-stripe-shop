e2501fc3ccb685edd5fd4c0773d80a37a70d29d7
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-07-13 17:02:14 +0100

    before pulling in data from firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,14 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgCyO2LaqAIteV_NSmrEqa_oZsVblAg5s",
@@ -31,6 +38,25 @@ googleProvider.setCustomParameters({
 const facebookProvider = new FacebookAuthProvider();
 
 export const db = getFirestore();
+
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("DONE");
+};
+
+
 
 export const auth = getAuth();
 
