3dbf083c693e1a0f93f3d6664213e03a8574a354
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-07-15 16:30:36 +0100

    updating class with className

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -23,19 +23,19 @@ const Authentication = () => {
           (
             <div className="w-screen h-screen absolute bg-white bg-opacity-50 flex justify-center items-center top-0 left-0 z-100">
               <svg
-                class="checkmark"
+                className="checkmark"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 52 52"
               >
                 <circle
-                  class="checkmark__circle"
+                  className="checkmark__circle"
                   cx="26"
                   cy="26"
                   r="25"
                   fill="none"
                 />
                 <path
-                  class="checkmark__check"
+                  className="checkmark__check"
                   fill="none"
                   d="M14.1 27.2l7.1 7.2 16.7-16.8"
                 />
